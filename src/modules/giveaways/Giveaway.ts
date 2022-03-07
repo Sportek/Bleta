5586f8f8224af6250ae7a8dc6b559bca7f5a9315
Sportek <57534192+Sportek@users.noreply.github.com>
2022-03-06 22:33:18 -0500

    Premier commit

--- src/modules/giveaways/Giveaway.ts
+++
@@ -1,9 +0,0 @@
-import { InterfaceGiveawayConfiguration } from "App/modules/giveaways/InterfaceGiveawayConfiguration";
-import GiveawayConfiguration from "App/modules/giveaways/configuration/Configuration.json";
-
-export class Giveaway {
-  private static giveawayConfiguration: InterfaceGiveawayConfiguration = GiveawayConfiguration
-  public static getConfiguration() {
-    return this.giveawayConfiguration
-  }
-}
--- src/modules/ranking/Ranking.ts
+++
@@ -1,9 +0,0 @@
-import RankingConfiguration from "App/modules/ranking/configuration/Configuration.json";
-import { InterfaceRankingConfiguration } from "App/modules/ranking/InterfaceRankingConfiguration";
-
-export class Ranking {
-  private static rankingConfiguration: InterfaceRankingConfiguration = RankingConfiguration
-  public static getConfiguration() {
-    return this.rankingConfiguration
-  }
-}
